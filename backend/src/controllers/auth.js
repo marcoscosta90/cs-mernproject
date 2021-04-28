dfcc4340beb57943b159de2c484f231185df915b
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-04-28 17:13:26 -0300

    new model category

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -65,12 +65,4 @@ exports.signin = (req, res) => {
       });
     }
   });
-};
-
-exports.requireSignin = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-  const user = jwt.verify(token, process.env.JWT_SECRET);
-
-  req.user = user;
-  next();
-};
+}
